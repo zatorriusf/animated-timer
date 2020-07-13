909b155d4398fc1f11b04b3ef9088a0d090b790b
zatorriusf <zatorrius.furniss@outlook.com>
2020-07-13 11:00:20 -0400

    Created setDuration method in Timer class
    created timer object based on index.html elements

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ class Timer{
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
 
-        this.startBtn.addEventListener('click',this.start());
-        this.pauseBtn.addEventListener('click',this.pauseBtn());
+        this.startBtn.addEventListener('click',this.start);
+        this.pauseBtn.addEventListener('click',this.pause);
     }
     
     start(){
@@ -14,4 +14,12 @@ class Timer{
     pause(){
         console.log(`let's pause here for a second`)
     }
+    setDuration(e){
+        
+    }
 }
+
+const duration = document.querySelector('#duration');
+const startbtn = document.querySelector('#start');
+const pausebtn = document.querySelector('#pause');
+const timer = new Timer(duration,startbtn,pausebtn);
