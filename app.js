13f724986b5ef0d82c4e47cab0e10225d4be20f8
zatorriusf <zatorrius.furniss@outlook.com>
2020-07-13 15:23:43 -0400

    Multiple Changes:
    Update the duration prop to durationInput as
    it references the html input element
    
    completed the tick method now subtracting one per second
    created method setDuration to allow for user input countdown
    created method updateTimer to display the updated remaining time

--- app.js
+++ app.js
@@ -1,44 +1,50 @@
 class Timer{
-    constructor(duration, startBtn, pauseBtn){
-        this.durationInput = duration;
+    constructor(durationInput, startBtn, pauseBtn){
+        this.durationInput = durationInput;
         this.startBtn = startBtn;
         this.pauseBtn = pauseBtn;
         this.running = false;
+        
 
         this.startBtn.addEventListener('click',this.start);
         this.pauseBtn.addEventListener('click',this.pause);
+        this.setDuration();
     }
     
     start = () => {
         if(!this.running){
-            console.log(`starting the timer`);
             this.running = !this.running;
             this.durationInput.disabled = true;
+            this.setDuration(this.durationInput.value);
             this.tick();
             this.tickId = setInterval(this.tick,1000);
-        } else {
-            console.log(`The timer is already running`);
         }
     }
     pause = () => {
         if(this.running){
-            console.log(`let's pause here`);
             this.durationInput.disabled = false;
             clearInterval(this.tickId);
             this.running = !this.running;
-        } else {
-            console.log(`cannot pause what isn't running`)
         }
     }
-    setDuration = (e) => {
-
+    setDuration = (value = 60) => {
+        this.duration = value;
+        this.updateTimer();
     }
     tick = () =>{
-        console.log(`tick`);
+        this.duration -= 1;
+        this.updateTimer(); 
+        if(this.duration === 0){
+            this.setDuration();
+        }
+    } 
+
+    updateTimer(){
+        this.durationInput.value = this.duration;
     }
 }
 
-const duration = document.querySelector('#duration');
+const durationInput = document.querySelector('#duration');
 const startbtn = document.querySelector('#start');
 const pausebtn = document.querySelector('#pause');
-const timer = new Timer(duration,startbtn,pausebtn);
+const timer = new Timer(durationInput,startbtn,pausebtn);
