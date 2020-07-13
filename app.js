849677dda5c0f92e5455591174712f1dcb201373
zatorriusf <zatorrius.furniss@outlook.com>
2020-07-13 16:29:11 -0400

    Adding changes to create svg circle

--- app.js
+++ app.js
@@ -2,3 +2,12 @@ const durationInput = document.querySelector('#duration');
 const startbtn = document.querySelector('#start');
 const pausebtn = document.querySelector('#pause');
 const timer = new Timer(durationInput,startbtn,pausebtn);
+
+const circle = document.querySelector('circle');
+const circleCenter = [200,200];
+const circleRadius = 190;
+const circleStrokeWidth = 10;
+circle.setAttribute('r', circleRadius);
+circle.setAttribute('cx',circleCenter[0]);
+circle.setAttribute('cy',circleCenter[1]);
+circle.setAttribute('stroke-width', circleStrokeWidth);
